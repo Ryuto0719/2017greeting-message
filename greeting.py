eb29cdf53ce6472a6e665a8a0b7345f027f11c53
Ryuto0719 <105907213+Ryuto0719@users.noreply.github.com>
2023-06-19 14:53:52 +0900

    a
    a

--- greeting.py
+++ greeting.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def greet():
     hour = datetime.now().hour
     if hour <= 11:
-        message = 'Good morning'+ name + '-san!'
+        message = 'Hello'+ name + '-san!'
     elif hour <= 17:
         message = 'Hello'+ name + '-san!'
     else:
